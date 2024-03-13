00b5e435cc0b9f1debb015d7e9876f50b757de4a
tejaswi <tejaswikurapati88@gmail.com>
2024-03-13 15:36:46 +0000

    explained uuid, immutable state

---
+++ src/components/ContactItem/index.js
@@ -0,0 +1,33 @@
+import './index.css'
+
+const ContactItem = props => {
+  const {contactDetails, toggleisFavoarite} = props
+  const {name, mobileNo, isFavorite, id} = contactDetails
+  const onClickFavorite = () => {
+    toggleisFavoarite(id)
+  }
+  const starImgUrl = isFavorite
+    ? 'https://assets.ccbp.in/frontend/react-js/star-filled-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/star-outline-img.png'
+
+  return (
+    <li className="table-row">
+      <div className="table-cell name-column">
+        <p>{name}</p>
+      </div>
+      <hr className="separator" />
+      <div className="table-cell mobile-no-column">
+        <p className="mobile-no-value">{mobileNo}</p>
+        <button
+          type="button"
+          onClick={onClickFavorite}
+          className="favorite-icon-container"
+        >
+          <img src={starImgUrl} className="favorite-icon" alt="star" />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default ContactItem
